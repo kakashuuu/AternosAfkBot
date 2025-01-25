8e9b50618b79af8ecc38e49a6c8da64873965a32
Ilovetoes <74071099+Ilovetoes@users.noreply.github.com>
2025-01-25 13:42:29 +0000

    Aternos AFK Bot working

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'villa2027.aternos.me',
     version: '1.16.5', // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-    username: 'zakifr',
+    username: 'zakifrfr',
     port: 51113,
     plugins: [AutoAuth],
     AutoAuth: 'bot112022'
